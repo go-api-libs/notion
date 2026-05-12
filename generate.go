58e211b64041ef05867b3c52848598264866063e
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-05-13 01:25:58 +0200

    Update generate.go

--- generate.go
+++ generate.go
@@ -1,7 +1,6 @@
 package notion
 
-//go:generate go tool openapi-enrich
-
-// TODO: flatten, compress
-
+// TODO: go:generate go tool openapi-enrich
+// TODO: go:generate go tool openapi-flatten
+// TODO: go:generate go tool openapi-compress
 //go:generate go tool openapi-codegen -client
